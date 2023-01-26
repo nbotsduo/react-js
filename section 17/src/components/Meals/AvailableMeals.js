b8e1aa7aed17eefbea9fe9331dc857e6fe60908e
Naim <mnaimhatta@yahoo.com>
2023-01-26 15:36:43 +0800

    test

--- section 17/src/components/Meals/AvailableMeals.js
+++ section 17/src/components/Meals/AvailableMeals.js
@@ -1,54 +1,28 @@
-import Card from "../UI/Card";
-import MealItem from "./MealItem/MealItem";
-import classes from "./AvailableMeals.module.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-// const DUMMY_MEALS = [
-//   {
-//     id: 'm1',
-//     name: 'Sushi',
-//     description: 'Finest fish and veggies',
-//     price: 22.99,
-//   },
-//   {
-//     id: 'm2',
-//     name: 'Schnitzel',
-//     description: 'A german specialty!',
-//     price: 16.5,
-//   },
-//   {
-//     id: 'm3',
-//     name: 'Barbecue Burger',
-//     description: 'American, raw, meaty',
-//     price: 12.99,
-//   },
-//   {
-//     id: 'm4',
-//     name: 'Green Bowl',
-//     description: 'Healthy...and green...',
-//     price: 18.99,
-//   },
-// ];
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [httpError, setHttpError] = useState(null);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
-      setIsLoading(true);
       const response = await fetch(
-        "https://react-http-fc252-default-rtdb.firebaseio.com/meals.json"
+        'https://react-http-fc252-default-rtdb.firebaseio.com/meals.json'
       );
 
       if (!response.ok) {
-        throw new Error("Something went wrong!");
+        throw new Error('Something went wrong!');
       }
 
       const responseData = await response.json();
 
       const loadedMeals = [];
+
       for (const key in responseData) {
         loadedMeals.push({
           id: key,
@@ -57,13 +31,14 @@ const AvailableMeals = () => {
           price: responseData[key].price,
         });
       }
+
       setMeals(loadedMeals);
       setIsLoading(false);
     };
 
     fetchMeals().catch((error) => {
       setIsLoading(false);
-      setHttpError(error);
+      setHttpError(error.message);
     });
   }, []);
 
@@ -82,6 +57,7 @@ const AvailableMeals = () => {
       </section>
     );
   }
+
   const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
