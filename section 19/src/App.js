e1cf4b83ed552560ccbad98cdd13660908429ee5
Naim <mnaimhatta@yahoo.com>
2023-02-15 12:19:14 +0800

    sec 19

--- section 19/src/App.js
+++ section 19/src/App.js
@@ -1,17 +1,70 @@
-import { useSelector } from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { Fragment, useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { uiAction } from "./store/ui-slice";
 
 function App() {
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
 
-  const showCart =useSelector(state => state.ui.cartIsVisible);
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiAction.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://react-http-fc252-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+
+      dispatch(
+        uiAction.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Send cart data successfully!",
+        })
+      );
+    };
+    sendCartData.catch((error) => {
+      dispatch(
+        uiAction.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Send cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- section 19/src/store/cart-slice.js
+++ section 19/src/store/cart-slice.js
@@ -1,23 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const cartSlice = createSlice({
-  name: "cart",
+  name: 'cart',
   initialState: {
     items: [],
     totalQuantity: 0,
   },
   reducers: {
-    addItem(state, action) {
+    replaceCart(state,action){
+      state.totalQuantity=action.payload.totalQuantity;
+      state.items=action.payload.items;
+    },
+    addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          name: newItem.title,
+          name: newItem.title
         });
       } else {
         existingItem.quantity++;
@@ -26,10 +30,10 @@ const cartSlice = createSlice({
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
-      const existingItem = state.items.find((item) => item.id === id);
+      const existingItem = state.items.find(item => item.id === id);
       state.totalQuantity--;
       if (existingItem.quantity === 1) {
-        state.items = state.items.filter((item) => item.id !== id);
+        state.items = state.items.filter(item => item.id !== id);
       } else {
         existingItem.quantity--;
         existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
@@ -38,5 +42,6 @@ const cartSlice = createSlice({
   },
 });
 
-export const cartActions =cartSlice.actions;
+export const cartActions = cartSlice.actions;
+
 export default cartSlice;
