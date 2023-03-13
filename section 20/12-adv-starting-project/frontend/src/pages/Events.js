1eef5b9c9f26cb7a8d09795560ff838267eb15f0
Naim <mnaimhatta@yahoo.com>
2023-03-13 16:06:53 +0800

    ll

---
+++ section 20/12-adv-starting-project/frontend/src/pages/Events.js
@@ -0,0 +1,34 @@
+import { json, useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
+
+function EventsPage() {
+  const data = useLoaderData();
+
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
+
+  const events = data.events;
+  return <EventsList events={events} />;
+}
+
+export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // throw { message:'Could not fetch events.'};
+    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    //   status: 500,
+    // });
+    return json(
+      { message: "Could not fetch events." },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    return response;
+  }
+}
