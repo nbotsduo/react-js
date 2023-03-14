0f346fb1d11a994e7fd5fb005bc84cbe6c7cd81b
Naim <mnaimhatta@yahoo.com>
2023-03-14 12:51:52 +0800

    1

--- section 20/12-adv-starting-project/frontend/src/pages/EventDetail.js
+++ section 20/12-adv-starting-project/frontend/src/pages/EventDetail.js
@@ -1,11 +1,11 @@
-import { json, redirect, useRouteLoaderData } from "react-router-dom";
-import EventItem from "../components/EventItem";
+import { useRouteLoaderData, json,redirect } from 'react-router-dom';
+
+import EventItem from '../components/EventItem';
 
 function EventDetailPage() {
-  
   const data = useRouteLoaderData('event-detail');
 
-  return <EventItem event={data.event}/>;
+  return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
@@ -13,21 +13,26 @@ export default EventDetailPage;
 export async function loader({ request, params }) {
   const id = params.eventId;
 
-  const response = await fetch("http://localhost:8080/events/" + id);
+  const response = await fetch('http://localhost:8080/events/' + id);
 
   if (!response.ok) {
     throw json(
-      { message: "Could not fetch details for selected event." },
-      { status: 500 }
+      { message: 'Could not fetch details for selected event.' },
+      {
+        status: 500,
+      }
     );
   } else {
     return response;
   }
 }
 
-export async function action ({params}){
+export async function action ({params,request}){
   const eventId = params.eventId;
-  const response = await fetch('http://localhost:8080/events/' + eventId);
+  const response = await fetch('http://localhost:8080/events/' + eventId,{
+    method:request.method,
+
+  });
 
   if (!response.ok) {
     throw json(
--- section 20/12-adv-starting-project/frontend/src/pages/NewEvent.js
+++ section 20/12-adv-starting-project/frontend/src/pages/NewEvent.js
@@ -1,5 +1,6 @@
-import { json, redirect } from "react-router-dom";
-import EventForm from "../components/EventForm";
+import { json, redirect } from 'react-router-dom';
+
+import EventForm from '../components/EventForm';
 
 function NewEventPage() {
   return <EventForm />;
@@ -11,23 +12,23 @@ export async function action({ request, params }) {
   const data = await request.formData();
 
   const eventData = {
-    title: data.get("title"),
-    image: data.get("image"),
-    date: data.get("date"),
-    description: data.get("description"),
+    title: data.get('title'),
+    image: data.get('image'),
+    date: data.get('date'),
+    description: data.get('description'),
   };
 
-  const response = fetch("http://localhost:8080/events", {
-    method: "POST",
+  const response = await fetch('http://localhost:8080/events', {
+    method: 'POST',
     headers: {
-      "Content-type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(eventData),
   });
 
   if (!response.ok) {
-    throw json({ message: "Could not save event" }, { status: 500 });
+    throw json({ message: 'Could not save event.' }, { status: 500 });
   }
 
-  return redirect('/events')
+  return redirect('/events');
 }
