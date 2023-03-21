5256b86112c9db3a5279f92a329f23e323d9a0cc
Naim <mnaimhatta@yahoo.com>
2023-03-21 17:02:37 +0800

    token

--- section 21/01-starting-project/frontend/src/components/MainNavigation.js
+++ section 21/01-starting-project/frontend/src/components/MainNavigation.js
@@ -1,7 +1,7 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
-import classes from "./MainNavigation.module.css";
-import NewsletterSignup from "./NewsletterSignup";
+import classes from './MainNavigation.module.css';
+import NewsletterSignup from './NewsletterSignup';
 
 function MainNavigation() {
   return (
@@ -29,7 +29,6 @@ function MainNavigation() {
               Events
             </NavLink>
           </li>
-
           <li>
             <NavLink
               to="/newsletter"
@@ -42,7 +41,7 @@ function MainNavigation() {
           </li>
           <li>
             <NavLink
-              to="/auth"
+              to="/auth?mode=login"
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
