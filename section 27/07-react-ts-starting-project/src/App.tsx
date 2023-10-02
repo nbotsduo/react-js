fae4bad993a341d5af7d44d96b7edc9fca8cf322
Naim <m.naim@alami.my>
2023-10-02 15:01:46 +0800

    rhrt

--- section 27/07-react-ts-starting-project/src/App.tsx
+++ section 27/07-react-ts-starting-project/src/App.tsx
@@ -1,16 +1,32 @@
-import NewTodo from './components/NewTodo';
-import Todos from './components/Todos';
-import Todo from './models/todo';
+import { useState } from "react";
+import NewTodo from "./components/NewTodo";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
-  const todos = [
-    new Todo('Learn rx'),
-    new Todo('Learn ts')
-  ]
+  // const todos = [
+  //   new Todo('Learn rx'),
+  //   new Todo('Learn ts')
+  // ]
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const addToDoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
+const removeTodoHandler=(todoId:string)=>{
+  setTodos((prevTodos)=>{
+    return prevTodos.filter(todo => todo.id !== todoId)
+  })
+}
+
   return (
     <div className="App">
-      <NewTodo />
-      <Todos items={todos}/>
+      <NewTodo onAddTodo={addToDoHandler} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler}/>
     </div>
   );
 }
--- section 27/07-react-ts-starting-project/src/components/NewTodo.tsx
+++ section 27/07-react-ts-starting-project/src/components/NewTodo.tsx
@@ -1,22 +1,26 @@
-import React, { useRef } from "react";
+import { useRef } from 'react';
+import classes from './NewTodo.module.css';
 
-const NewTodo :React.FC<{onAddTodo:(text:string)=>void}>= (props) => {
-    const todoTextInputRef = useRef<HTMLInputElement>(null);
+const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
 
-    const enteredText= todoTextInputRef.current?.value;
-    if(enteredText?.trim().length === 0){
-        return;
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
     }
-    
+
     props.onAddTodo(enteredText);
   };
+
   return (
-    <form onSubmit={submitHandler}>
-      <label htmlFor="text">To do text</label>
-      <input type="text" id="text" ref={todoTextInputRef}/>
+    <form onSubmit={submitHandler} className={classes.form}>
+      <label htmlFor='text'>Todo text</label>
+      <input type='text' id='text' ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
